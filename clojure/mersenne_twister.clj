6305357b50b1270ca3b580cb7cd7168281b7b4d7
Steve Knight <stkni@shuttle.(none)>
2009-08-21 15:08:12 +0100

    Stage 1.
    Got a working reference implementation of the twister.

---
+++ clojure/mersenne_twister.clj
@@ -0,0 +1,100 @@
+;;;; A Clojure implementation of the Mersenne Twister algorithm  
+;;;;
+;;;; Copyright (c) 2009 Steve Knight <stkni@gmail.com>
+;;;;
+;;;; Copyright (C) 1997, 1999 Makoto Matsumoto and Takuji Nishimura. 
+;;;; matumoto@math.keio.ac.jp    
+;;;; 
+;;;; Permission is hereby granted, free of charge, to any person obtaining
+;;;; a copy of this software and associated documentation files (the
+;;;; "Software"), to deal in the Software without restriction, including
+;;;; without limitation the rights to use, copy, modify, merge, publish,
+;;;; distribute, sublicense, and/or sell copies of the Software, and to
+;;;; permit persons to whom the Software is furnished to do so, subject to
+;;;; the following conditions:
+;;;; 
+;;;; The above copyright notice and this permission notice shall be
+;;;; included in all copies or substantial portions of the Software.
+;;;; 
+;;;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+;;;; EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+;;;; MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+;;;; NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+;;;; LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+;;;; OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+;;;; WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+;;;;
+                                    
+(ns com.hackinghat.common-library.mersenne-twister)
+
+(def n 624)
+(def m 397)
+(def matrix-a 0x9908b0df)  					; constant vector a
+(def upper-mask 0x80000000)	 	  		; most significant w-r bits
+(def lower-mask 0x7fffffff)	 	  		; least significant w-r bits
+(def tempering-mask-b 0x9d2c5680)
+(def tempering-mask-c 0xefc60000)
+(defn tempering-shift-u [y] (bit-shift-right y 11))
+(defn tempering-shift-s [y] (bit-shift-left y 7))
+(defn tempering-shift-t [y] (bit-shift-left y 15))
+(defn tempering-shift-l [y] (bit-shift-right y 18))
+
+(def mt  (ref []))
+(def mti (ref (inc n)))
+
+(defn reset []
+	(dosync 
+		(ref-set mt [])
+		(ref-set mti (inc n)))
+	nil)
+		
+(defn sgenrand [seed]
+	(dosync
+		(ref-set mt [])
+		(loop [i 0 s seed]
+			(if (< i n)
+				(let [mti     (bit-and s 0xffff0000)
+				      s-1     (inc (* 69069 s))]
+				   (ref-set mt (into @mt [(bit-or mti (bit-shift-right (bit-and s-1 0xffff0000) 16))]))
+				   (recur (inc i) (inc (* 69069 s-1))))))
+		(ref-set mti n)))
+
+(defn _genrand [v1 v2 v3]
+	(let [y (bit-or (bit-and v1 upper-mask)
+	                (bit-and v2 lower-mask))]
+	  (bit-xor (bit-xor v3 
+	                    (bit-shift-right y 1))
+	           (if (= 0 (bit-and y 0x1))
+	              0
+	              matrix-a))))
+	              
+;; Return the next element from the mt array
+(defn genrand-n []
+ 	(dosync
+		(if 
+		  (>= @mti n)
+				(do
+					(if (= @mti (inc n))
+						(sgenrand 4357))
+				  (ref-set mt  (loop [k 0 v []]
+							 					 (if (< k n)
+								 						(let [v-1 (cond 
+								 						            (< k (- n m)) (conj v (_genrand (nth @mt k) (nth @mt (inc k)) (nth @mt (+ k m))))
+						 				   					  			(< k (dec n)) (conj v (_genrand (nth @mt k) (nth @mt (inc k)) (nth v (+ k (- m n)))))
+						 				              			(= k (dec n)) (conj v (_genrand (nth @mt (dec n)) (nth v 0) (nth v (dec m)))))]
+						                 (recur (inc k) v-1))
+						                v)))
+					 (ref-set mti 0)))
+			;; Now return the element and advance the counter
+		  (let [retval (nth @mt @mti)]
+				(ref-set mti (inc @mti))
+				retval)))
+   
+(defn genrand []
+		(let [y (genrand-n)
+		      y1 (bit-xor y (tempering-shift-u y))
+		      y2 (bit-xor y1 (bit-and (tempering-shift-s y1) tempering-mask-b))
+		      y3 (bit-xor y2 (bit-and (tempering-shift-t y2) tempering-mask-c))
+		      y4 (bit-xor y3 (tempering-shift-l y3))]
+		   y4))
+		  
